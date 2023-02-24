cf0abce6c9e453cd9cc78a3f79c666632655fc71
kkoonz <kkoon79@gmail.com>
2023-02-24 17:22:58 +0900

    init

---
+++ backend/src/main/java/net/kkoon/chips/ChipsApplication.java
@@ -0,0 +1,13 @@
+package net.kkoon.chips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/net/kkoon/chips/ChipsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kkoon.chips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
